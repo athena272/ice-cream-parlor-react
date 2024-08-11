bc4df2c344a90729a75c88ab22fcfa2c45ba10f0
athena272 <guilhermera272@gmail.com>
2024-08-11 13:51:47 -0300

    Create RoutesPage.tsx

---
+++ src/Routes/RoutesPage.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import HomePage from '../Pages/HomePage/HomePage';
+import FlavorsPage from '../Pages/FlavorsPage/FlavorsPage';
+import AboutPage from '../Pages/AboutPage/AboutPage';
+
+const RoutesPage = () => (
+    <Routes> {/* Seleciona apenas um componente para exibir na tela baseado na url */}
+        <Route path='/' element={<HomePage />} /> {/* Vomponente que associa a rota ao componente */}
+        <Route path='/flavors' element={<FlavorsPage />} />
+        <Route path='/about' element={<AboutPage />} />
+    </Routes>
+);
+
+export default RoutesPage;
